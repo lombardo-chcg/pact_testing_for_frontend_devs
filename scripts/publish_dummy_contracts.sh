0fcf1933eae40dfcb1d7c9133fbea91dbe83ee9f
Dominick Lombardo <dpa.lombardo@gmail.com>
2017-02-11 20:24:58 -0600

    get publish script working

--- scripts/publish_dummy_contracts.sh
+++ scripts/publish_dummy_contracts.sh
@@ -3,11 +3,18 @@
 dockerHost=$(docker-machine ip dev)
 
 for contract in frontend/dummy_data/*; do
-  consumer=$(jq ."consumer"."name" < $contract)
-  provider=$(jq ."provider"."name" < $contract)
+  consumer=$(jq ."consumer"."name" < $contract | tr -d '"')
+  provider=$(jq ."provider"."name" < $contract | tr -d '"')
 
   curl \
+    -s \
     -X PUT \
-    -d $contract \
-    $dockerHost
+    -H "Content-Type: application/json" \
+    -d @$contract \
+    http://$dockerHost:80/pacts/provider/$provider/consumer/$consumer/version/1 \
+    1>/dev/null
+
+  [[ $? -gt 0 ]] && { echo "error while publishing $consumer $provider contract"; exit 1; }
+
+  echo -e "\nSuccessfully published contract between $consumer and $provider"
 done
